1daf06ed1eaba4effa75ff978dbd8f4af4a8d3e4
Lornepritchett1@gmail.com <lornepritchett1gmail.com@Lornes-MacBook-Pro.local>
2026-01-10 18:36:37 -0330

    Force unregister legacy service worker

---
+++ sw.js
@@ -0,0 +1,14 @@
+self.addEventListener('install', event => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    self.registration.unregister().then(() => {
+      return self.clients.matchAll();
+    }).then(clients => {
+      clients.forEach(client => client.navigate(client.url));
+    })
+  );
+});
+
